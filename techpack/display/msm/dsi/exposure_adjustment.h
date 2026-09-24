a30e12382d1354a9d07a34b390af22712de4c596
ushion0a <1.next632@passfwd.com>
2026-09-25 04:54:07 +0800

    techpack: display: declare exposure control interface

---
+++ techpack/display/msm/dsi/exposure_adjustment.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef _DSI_EXPOSURE_ADJUSTMENT_H_
+#define _DSI_EXPOSURE_ADJUSTMENT_H_
+
+#include <linux/types.h>
+
+#define EA_ELVSS_OFF_THRESHOLD 1024
+#define EA_PCC_MIN 580
+#define EA_PCC_MAX 32768
+
+struct dsi_panel;
+
+bool ea_panel_is_enabled(struct dsi_panel *panel);
+int ea_panel_mode_ctrl(struct dsi_panel *panel, bool enable);
+u32 ea_panel_calc_backlight(struct dsi_panel *panel, u32 level);
+u32 ea_panel_get_coefficient(void);
+
+#endif
